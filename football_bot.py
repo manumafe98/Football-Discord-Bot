4c3731ffd73591a3e400c8d8ead7c109c1a2da9c
manumafe98 <manumaxerapro@gmail.com>
2023-07-31 20:14:06 -0300

    Adding an instructions command and formatting the matches message

--- football_bot.py
+++ football_bot.py
@@ -1,14 +1,18 @@
 import discord
 from discord.ext import commands
 import requests
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 from table2ascii import table2ascii as t2a, PresetStyle
 
 
 # Football api: https://www.football-data.org/documentation/quickstart
 # Discord documentation: https://discordpy.readthedocs.io/en/stable/
+
 # Message look like a table: https://stackoverflow.com/questions/63565825/how-to-make-data-to-be-shown-in-tabular-form-in-discord-py
 # Commands discord docu: https://discordpy.readthedocs.io/en/stable/ext/commands/commands.html
+# Send an image response: https://stackoverflow.com/questions/63100479/multiple-photos-in-discord-py-embed
+# embed: https://leovoel.github.io/embed-visualizer/
+
 
 CURRENT_DATE = date.today()
 
@@ -31,24 +35,40 @@ endpoint_params = {
 }
 
 
+@bot.command(name="instructions")
+async def help(ctx):
+    embed = discord.Embed(title="Instructions", colour=discord.Colour(0xca024), 
+                          description="This bot gives you the next match of the inputed team.")
+
+    embed.add_field(name="list_teams", 
+                    value="This command expects one of the top 5 leagues as parameters of the command, and as output gives you a list of all the teams of that league with their respective ids.\nExample: $list_teams premier_league")
+    
+    embed.add_field(name="next_match", 
+                    value="With the previously gotten id you can get the next match of that team.\nExample: $list_match 61")
+
+    await ctx.send(embed=embed)
+
+
 # Get top 5 leagues teams with id, league name and league id
 @bot.command(name="list_teams")
-async def list_teams(ctx):
+async def list_teams(ctx, inputed_league):
     body_array = []
 
     for league in top_5_ligues_array:
-        response = requests.get(f"https://api.football-data.org/v4/competitions/{list(league.values())[0]}/teams", 
-                                headers=endpoint_headers)
-        teams = response.json()["teams"]
-        for team in teams:
-            body_array.append([team["name"], team["id"], list(league.keys())[0], list(league.values())[0]])
+        if list(league.keys())[0] == inputed_league:
+            response = requests.get(f"https://api.football-data.org/v4/competitions/{list(league.values())[0]}/teams", 
+                                    headers=endpoint_headers)
+            teams = response.json()["teams"]
+            for team in teams:
+                body_array.append([team["name"], team["id"]])
 
     table = t2a(
-        header=["Team", "Team Id", "League", "League Id"],
+        header=["Team", "Team Id"],
         body=body_array,
         style=PresetStyle.thin_compact
     )
-    await ctx.send(table)
+
+    await ctx.send(f"```\n{table}\n```")
 
 
 # Get Team next match
@@ -64,6 +84,28 @@ async def list_teams(ctx, team_id):
     away_team_logo = output["matches"][0]["awayTeam"]["crest"]
     away_team_name = output["matches"][0]["awayTeam"]["name"]
 
-    await ctx.send(f"{home_team_name} vs {away_team_name}")
 
-bot.run("roken")
+    timestamp = output["matches"][0]["utcDate"]
+    datetime_obj = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
+    formatted_date = datetime_obj.strftime("%d/%m")
+
+    r = requests.get(f"https://api.football-data.org/v4/teams/{team_id}", headers=endpoint_headers)
+    team_name = r.json()["name"]
+
+    embed = discord.Embed(title=f"{team_name} next match is on the day {formatted_date}", 
+                          colour=discord.Colour(0x4be080), url="https://manumafe98.github.io/cv/")
+
+    embed.add_field(name="Home Team", value=home_team_name, inline=True)
+    embed.add_field(name="Away Team", value=away_team_name, inline=True)
+    
+    
+    embed1 = discord.Embed(url="https://manumafe98.github.io/cv/")
+    embed2 = discord.Embed(url="https://manumafe98.github.io/cv/")
+
+    embed1.set_image(url=home_team_logo)
+    embed2.set_image(url=away_team_logo)
+
+    await ctx.send(embeds=[embed, embed1, embed2])
+    
+
+bot.run("MTEzNTY4NjU1Njk5MzIwODQ3MQ.G8xIoQ.dG8vTtmD_r3Oa19dvGGaqntY95E2KcgJkBzHeQ")
